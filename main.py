18a32214325b66bf31dbe32a2be25402cb4c1169
algorismwebmaster <webmaster@algorismng.com>
2022-01-04 12:43:25 +0100

    fleshing out api requests

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,14 +11,16 @@ all_posts = [
     {"id":2, "title": "Postiscus", "content": "Post no 2 should be the most entertaining and exciting due to a number of irreconciliable factors", "author": "admin", "published": True }
 ]
 
+def find_post(id):
+    return [i for i in all_posts if int(id) == i["id"]]
 
-# class Post(BaseModel):
-#     id: int
-#     title: str
-#     content: str
-#     author: str = "Admin"
-#     published: bool = True
-#     # rating: Optional[int] = None
+
+class Post(BaseModel):
+    title: str
+    content: str
+    author: str = "Admin"
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -29,6 +33,7 @@ def greet_ibrahim():
     return {"greetings": "Hello there!, Ibrahim!!!"}
 
 
+# get all posts
 @app.get("/posts")
 def get_posts():
     return {
@@ -36,7 +41,18 @@ def get_posts():
     }
 
 
+# Create a single post
 @app.post("/posts")
-def create_post(payload: dict = Body(...)):
-    print("post payload", payload)
-    return {"data": payload}
+def create_post(payload: Post):
+    # payload["id"] = randrange(1, 500)
+    payload_dict = payload.dict()
+    payload_dict["id"] = randrange(1, 500)
+    all_posts.append(payload_dict)
+    return {"all_posts": all_posts}
+
+
+# Get a single post
+@app.get("/posts/{id}")
+def get_single_post(id: int):
+    data = find_post(id)
+    return {"data": data}
